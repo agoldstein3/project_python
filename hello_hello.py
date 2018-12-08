7b31cf2b1248c73653ea27bf710a911d6d9a21de
agoldstein3 <45720462+agoldstein3@users.noreply.github.com>
2018-12-08 15:02:04 -0500

    Add files via upload

---
+++ hello_hello.py
@@ -0,0 +1,9 @@
+def print_favorite_number():
+    print("22")
+
+def say_introduction():
+    print("Hello, my name is Alex")
+    print("I am a meteorologist")
+
+print_favorite_number()
+say_introduction()
