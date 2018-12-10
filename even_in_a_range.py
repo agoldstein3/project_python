3161f04a3b8b041ac28806e8b5ff12f0f580b385
agoldstein3 <45720462+agoldstein3@users.noreply.github.com>
2018-12-10 00:11:24 -0500

    Update even_in_a_range.py

--- even_in_a_range.py
+++ even_in_a_range.py
@@ -1,3 +1,5 @@
+#this program tests whether a number 1) falls in the range [24,32]; and 2) is even.
+
 x = 33 # number to test
 print((x<33 and x>23) and x % 2 == 0) #set conditions; first condition gives range, second checks if round-off equals zero. 
                                       #if round-off equals zero, the value is even.
