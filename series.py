680f058fb13e27a4f448df06bf5178550e2e64c4
agoldstein3 <45720462+agoldstein3@users.noreply.github.com>
2018-12-10 14:03:36 -0500

    Update series.py

--- series.py
+++ series.py
@@ -1,6 +1,6 @@
 #this program calculates and prints the first 100 numbers in a series defined by the difference of two consecutive numbers increasing by one for each new number
-count = 1
-diff = 1
+count = 1 #initialize first number in series
+diff = 1 #initialize difference between two numbers in series
 
 while diff <= 100:
     print(count)
