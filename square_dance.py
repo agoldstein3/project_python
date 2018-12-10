ea2347af98fd05a0474d7522d39e29922bb2b973
agoldstein3 <45720462+agoldstein3@users.noreply.github.com>
2018-12-10 14:08:09 -0500

    Create square_dance.py

---
+++ square_dance.py
@@ -0,0 +1,11 @@
+#this program uses only "forward" and "left" functions to move a robot in a square
+
+from robotsim import left, forward, right, start_simulator
+start_simulator()
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
